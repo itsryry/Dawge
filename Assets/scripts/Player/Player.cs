cc0739381a2bf437009e75b097bb788b357ab2e6
Aminul Hasan <aminul.hasan90@gmail.com>
2015-12-14 22:08:11 -0800

    initial commit

---
+++ Assets/scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// SHould read off the setttings
+	public static float PLAYER_SPEED = 3f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotateX = -Input.acceleration.x;
+		rotateX = rotateX > 0 ? rotateX + PLAYER_SPEED : rotateX - PLAYER_SPEED;
+
+		this.transform.rotation *= Quaternion.Euler(0, 0, rotateX);
+	}
+
+
+}
